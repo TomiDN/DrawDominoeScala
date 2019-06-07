40c660591dc6f7c7d1f962baf50e1ac633c96946
Tomislav Nikolov <tomi_berk@abv.bg>
2019-06-08 01:35:11 +0300

    Update Validated.scala

--- Validated.scala
+++ Validated.scala
@@ -1,13 +1,10 @@
-package drawdominoes
+package drawdominoesgame
+
 
 sealed trait Validated[+E, +A]
 
+
 case class Valid[+A](a: A) extends Validated[Nothing, A]
 
+
 case class Invalid[+E](error: E) extends Validated[E, Nothing]
-
-object Invalid {
-
-  def apply[E](error: E): Invalid[E] = Invalid(error)
-
-}
