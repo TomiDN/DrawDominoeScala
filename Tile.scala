af05e495e7362364ef88ce5722115d6a8be4003d
Tomislav Nikolov <tomi_berk@abv.bg>
2019-06-14 01:48:31 +0300

    Update Tile.scala

--- Tile.scala
+++ Tile.scala
@@ -2,24 +2,15 @@ package drawdominoesgame
 
 
 class Tile(val a: Int, val b: Int) {
-
   def ==(t: Tile): Boolean = this.a == t.a && this.b == t.b
-
 
   def !=(t: Tile): Boolean = !(this == t)
 
-
   def print: String = s"[$a|$b] "
-
 }
-
 
 case class InvalidTile(c: Int = 7) extends Tile(c, c)
 
-
 object Tile {
-
   def apply(x: Int, y: Int): Tile = new Tile(x, y)
-
 }
-
