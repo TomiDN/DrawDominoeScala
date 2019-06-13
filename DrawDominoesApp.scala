d15e11cd34da9c983f79e8210f2da0fbb5ec788d
Tomislav Nikolov <tomi_berk@abv.bg>
2019-06-14 01:44:39 +0300

    Update DrawDominoesApp.scala

--- DrawDominoesApp.scala
+++ DrawDominoesApp.scala
@@ -1,58 +1,35 @@
 package drawdominoesgame
 
-import scala.io.StdIn
-
+import drawdominoesgame.io.Console._
+import drawdominoesgame.io.IO
 
 object DrawDominoesApp {
+  def readAnswer(message: String): IO[String] = for {
+    _ <- putStrLn(message)
+    input <- getStrLn
+  } yield input
 
-
-  def readAnswer(message: String): String = {
-
-    println(message)
-
-    StdIn.readLine
-
-  }
-
-
-  def identifyAnswer(input: String, answer1: String, answer2: String): Boolean =
-    if(input == answer1) true
-
+  def identifyAnswer(input: IO[String], answer1: String, answer2: String): Boolean =
+    if(input.unsafeRun() == answer1) true
     else false
-
 
   def showHighScore: Boolean = identifyAnswer(readAnswer(s"dominogame:~ Play game or see high scores?\n" +
     s"dominogame:~ ([-pg] for 'Play Game' and [-hs] for 'Show High Scores'): "),"-hs", "-pg")
 
-
-  def runGame(game: Game): Unit = {
-
+  def runGame(game: Game): IO[Unit] = {
     if (showHighScore) {
-
-      game.showHighScores()
-
+      game.showHighScores().unsafeRun()
       runGame(game)
-
     }else {
-
-      game.gameloop()
-
+      game.gameloop().unsafeRun()
       if (identifyAnswer(readAnswer(s"dominogame:~ Would you like to play again?\n" +
         s"dominogame:~ ([-y] for Yes and [-n] for No): "), "-y", "-n")) {
-
         runGame(game)
-
-      } else println(s"As you wish...")
-
+      } else putStrLn(s"As you wish...")
     }
-
   }
-
 
   def main(args: Array[String]): Unit = {
-
-    runGame(Game())
-
+    runGame(Game()).unsafeRun()
   }
-
 }
