717729dc02e1185604a50b75f2010f3e90abcda0
Tomislav Nikolov <tomi_berk@abv.bg>
2019-06-14 01:49:02 +0300

    Update Validated.scala

--- Validated.scala
+++ Validated.scala
@@ -3,8 +3,6 @@ package drawdominoesgame
 
 sealed trait Validated[+E, +A]
 
-
 case class Valid[+A](a: A) extends Validated[Nothing, A]
-
 
 case class Invalid[+E](error: E) extends Validated[E, Nothing]
