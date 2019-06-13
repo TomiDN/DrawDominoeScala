1cb0b194abde54e81473679dc8a10657337bc3c8
Tomislav Nikolov <tomi_berk@abv.bg>
2019-06-14 01:47:05 +0300

    Update HighScoring.scala

--- HighScoring.scala
+++ HighScoring.scala
@@ -1,67 +1,38 @@
 package drawdominoesgame
 
 import java.io._
-
 import scala.io.Source
+import drawdominoesgame.io.Console._
+import drawdominoesgame.io.IO
 
 
 class HighScoring(filename: String, printmessage: String, emptyfilemessage: String) {
-
   def orderingByScore(line: String): Int = line.drop(line.indexOfSlice(" by")).toInt
 
-
   def sortHighScores(savedHighScores: Vector[String]): Vector[String] = {
-
     val savedHighScores: Vector[String] = Source.fromFile(filename).getLines.toVector
-
     savedHighScores.sortBy(orderingByScore)
-
   }
 
-
   def writeSortedLines(sortedLines: Vector[String], writer: PrintWriter): Unit =
-
     if(sortedLines.nonEmpty) {
-
       writer.write(sortedLines.head)
-
       writeSortedLines(sortedLines.tail, writer)
-
     } else writer.close()
 
-
   def saveHighScore(player: String, score: Int): Unit =
-
     writeSortedLines(
       sortHighScores(Source.fromFile(filename).getLines.toVector :+ (score + " by " + player)),
       new PrintWriter(new File(filename))
     )
 
-
-  def printHighScore(): Unit =
-
-    println(printmessage)
-
+  def printHighScore(): IO[Unit] =
+    putStrLn(printmessage)
     if(Source.fromFile(filename).isEmpty){
-
-      println(emptyfilemessage)
-
+      putStrLn(emptyfilemessage)
     } else {
-
       for (line <- Source.fromFile(filename).getLines) {
-
-        println(line)
-
+        putStrLn(line)
       }
-
     }
-
-}
-
-object HighScoring {
-
-  def apply(filename: String, printmessage: String, emptyfilemessage: String): HighScoring =
-
-    new HighScoring(filename, printmessage, emptyfilemessage)
-
 }
