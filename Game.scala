9521dd25b767f865654dfd955b9a4e3b97da2bc1
Tomislav Nikolov <tomi_berk@abv.bg>
2019-06-14 15:49:41 +0300

    Update Game.scala

--- Game.scala
+++ Game.scala
@@ -72,7 +72,7 @@ class Game(val player1: Player,
             s"\n|  _         _  |" +
             s"\n| |_|       |_| |" +
             s"\n|_______________|\n"
-    case 6 =>  s"_______________" +
+    case 6 =>  s"_______________"  +
             s"\n|  _         _  |" +
             s"\n| |_|       |_| |" +
             s"\n|  _         _  |" +
@@ -84,18 +84,18 @@ class Game(val player1: Player,
   }
 
   def lastEnd: Tile = if (openends.isEmpty)
-    InvalidTile()
-  else openends.last
+      InvalidTile()
+    else openends.last
 
   def noMove: String = graphics(lastEnd.a) + graphics(lastEnd.a) + graphics(lastEnd.b)
 
   def emptyScreen: String = graphics(7) + graphics(7)
 
   def passMove(currentPlayer: Player): String = if (!boneyard.isEmpty)
-    s"dominogame:~ Boneyard still not empty and you can't pass a move!\n" +
-    s"dominogame:~ A new tile will be added to your pile instead!\n"      +
+      s"dominogame:~ Boneyard still not empty and you can't pass a move!\n" +
+      s"dominogame:~ A new tile will be added to your pile instead!\n"      +
       drawTile(currentPlayer)
-  else  noMove
+    else  noMove
 
   def newLast(newEnd: Tile): Tile = openends.moveToEnd(newEnd)
 
@@ -110,49 +110,50 @@ class Game(val player1: Player,
     case _ => openEndDisplay(openends.getAt(0))
   }
 
-  def nextOpenEndMenu(currentPlayer: Player): IO[Unit] = putStrLn(showNextOpenEnd           +
-                                                                  playerInfo(currentPlayer) +
-                                                                  s"dominogame:~  Again?([-y] - Yes, [-n] - No):")
+  def nextOpenEndMenu(currentPlayer: Player): IO[Unit] =  putStrLn(showNextOpenEnd  +
+                                                          playerInfo(currentPlayer) +
+                                                          s"dominogame:~  Again?([-y] - Yes, [-n] - No):")
 
   def pickNextOrNot(currentPlayer: Player): IO[String] = for {
     _ <- nextOpenEndMenu(currentPlayer)
     input <- getStrLn
   }yield input
 
-  def nextOpenEnd(currentPlayer: Player): String = pickNextOrNot(currentPlayer).unsafeRun() match {
-    case "-n" =>  identifyCommand(currentPlayer, "")
-    case _ => nextOpenEnd(currentPlayer)
-  }
+  def nextOpenEnd(currentPlayer: Player): String =
+    pickNextOrNot(currentPlayer).unsafeRun() match {
+      case "-n" =>  identifyCommand(currentPlayer, "")
+      case _ => nextOpenEnd(currentPlayer)
+    }
 
-  def wasTileDrawn(currentPlayer: Player): Validated[String, ArrayBuffer[Tile]] = currentPlayer.drawTileFromBN(boneyard)
-
-  def drawTile(currentPlayer: Player): String = wasTileDrawn(currentPlayer) match {
-    case Invalid(e) => "dominogame:~ " + e
-    case Valid(_) => noMove
-  }
+  def drawTile(currentPlayer: Player): String =
+    currentPlayer.drawTileFromBN(boneyard) match {
+      case Some(e) => "dominogame:~ " + e
+      case None => noMove
+    }
 
   def addOpenEnd(t: Tile): ArrayBuffer[Tile] = if (openends.isEmpty) {
-    if(t.a == t.b){
+      if(t.a == t.b){
+        openends.add(t)
+        openends.add(t)
+        openends.add(t)
+        openends.add(t)
+      } else {
+        openends.add(Tile(t.b, t.a))
+        openends.add(t)
+      }
+    } else if(t.a == t.b) {
       openends.add(t)
       openends.add(t)
       openends.add(t)
-      openends.add(t)
-    } else {
-      openends.add(Tile(t.b, t.a))
-      openends.add(t)
-    }
-  } else if(t.a == t.b) {
-    openends.add(t)
-    openends.add(t)
-    openends.add(t)
-  } else openends.add(t)
+    } else openends.add(t)
 
   def firstMove(t: Tile): String = {
     addOpenEnd(t)
     graphics(t.a) + graphics(t.b)
   }
 
-  def wrongMove(currentPlayer: Player): String = pickTile(currentPlayer, s"dominogame:~ Tile ends dismatch! Try again!\n")
+  def wrongMove(currentPlayer: Player): String =
+    pickTile(currentPlayer, s"dominogame:~ Tile ends dismatch! Try again!\n")
 
   def normalMove(t: Tile): String = {
     val last: String = graphics(lastEnd.b)
@@ -172,24 +173,27 @@ class Game(val player1: Player,
     else if(t.a != lastEnd.b && t.b != lastEnd.b) wrongMove(currentPlayer)
     else normalMove(t)
 
-  def checkReceivedIndex(validatedNum: Validated[String, Tile], currentPlayer: Player): String = validatedNum match {
-    case Invalid(e) => pickTile(currentPlayer, "dominogame:~ " + e + "\n")
-    case Valid(t) => analysingMove(t, currentPlayer)
-  }
+  def checkReceivedIndex(validatedNum: Validated[String, Tile], currentPlayer: Player): String =
+    validatedNum match {
+      case Invalid(e) => pickTile(currentPlayer, "dominogame:~ " + e + "\n")
+      case Valid(t) => analysingMove(t, currentPlayer)
+    }
 
-  def pickTile(currentPlayer: Player, message: String): String = toInt(readCommand(message).unsafeRun()) match {
-    case None => pickTile(currentPlayer, "")
-    case Some(num) => checkReceivedIndex(currentPlayer.drawTileFromPile(num), currentPlayer)
-  }
+  def pickTile(currentPlayer: Player, message: String): String =
+    toInt(readCommand(message).unsafeRun()) match {
+      case None => pickTile(currentPlayer, "")
+      case Some(num) => checkReceivedIndex(currentPlayer.drawTileFromPile(num), currentPlayer)
+    }
 
-  def identifyCommand(currentPlayer: Player, message: String):  String = readCommand(message).unsafeRun() match {
-    case "-pm" => passMove(currentPlayer)
-    case "-ne" => nextOpenEnd(currentPlayer)
-    case "-dt" => drawTile(currentPlayer)
-    case "-p" => pickTile(currentPlayer, "")
-    case "-q" => ""
-    case _ => identifyCommand(currentPlayer, message)
-  }
+  def identifyCommand(currentPlayer: Player, message: String):  String =
+    readCommand(message).unsafeRun() match {
+      case "-pm" => passMove(currentPlayer)
+      case "-ne" => nextOpenEnd(currentPlayer)
+      case "-dt" => drawTile(currentPlayer)
+      case "-p" => pickTile(currentPlayer, "")
+      case "-q" => ""
+      case _ => identifyCommand(currentPlayer, message)
+    }
 
   def readCommand(message: String): IO[String] = for {
     _ <- putStrLn(message + s"dominogame:~  Pick:")
@@ -200,26 +204,28 @@ class Game(val player1: Player,
 
   def playerInfo(currentPlayer: Player): String = currentPlayer.announcePlayer() + currentPlayer.printDeck() + "\n"
 
-  def pickMoveLoop(currentPlayer: Player, message: String): Boolean = identifyCommand(currentPlayer, message) match {
-    case "" => true
-    case visualisation: String =>
-      putStrLn(visualisation).unsafeRun()
-      Thread.sleep(1000)
-      putStrLn(graphics(7)).unsafeRun()
-      false
-  }
-
-  def playerScheduler( quit: Boolean, currentPlayer: Int): IO[Unit] = if (player1.pile.length > 0 && player2.pile.length > 0 && !quit) {
-    currentPlayer match {
-      case 1 => playerScheduler(pickMoveLoop(player1, playerInfo(player1)), 2)
-      case 2 => playerScheduler(pickMoveLoop(player2, playerInfo(player2)), 1)
+  def pickMoveLoop(currentPlayer: Player, message: String): Boolean =
+    identifyCommand(currentPlayer, message) match {
+      case "" => true
+      case visualisation: String =>
+        putStrLn(visualisation).unsafeRun()
+        Thread.sleep(1000)
+        putStrLn(graphics(7)).unsafeRun()
+        false
     }
-  } else  putStrLn(s"dominogame:~ Calculating results...\n\n")
+
+  def playerScheduler( quit: Boolean, currentPlayer: Int): IO[Unit] =
+    if (player1.pile.length > 0 && player2.pile.length > 0 && !quit) {
+      currentPlayer match {
+        case 1 => playerScheduler(pickMoveLoop(player1, playerInfo(player1)), 2)
+        case 2 => playerScheduler(pickMoveLoop(player2, playerInfo(player2)), 1)
+      }
+    } else  putStrLn(s"dominogame:~ Calculating results...\n\n")
 
   def isItHighScore(currentPlayer: Player): IO[Unit] = if(!boneyard.isEmpty) {
-    highScoreSystem.saveHighScore(currentPlayer.name, boneyard.length)
-    putStrLn(s"dominogame:~ You have reached a HIGH SCORE OF ${boneyard.length}!!!\n")
-  } else putStrLn(s"dominogame:~ A victory, indeed... but not a HIGH one!!!\n")
+      highScoreSystem.saveHighScore(currentPlayer.name, boneyard.length)
+      putStrLn(s"dominogame:~ You have reached a HIGH SCORE OF ${boneyard.length}!!!\n")
+    } else putStrLn(s"dominogame:~ A victory, indeed... but not a HIGH one!!!\n")
 
   def showHighScores(): IO[Unit] = highScoreSystem.printHighScore()
 
@@ -275,6 +281,6 @@ object Game {
     val boneyard: Deck = Deck(createBoneyard(ArrayBuffer()))
     instructions().unsafeRun()
     new Game(Player(readPlayerName(1).unsafeRun(), boneyard),
-             Player(readPlayerName(2).unsafeRun(), boneyard), boneyard)
+      Player(readPlayerName(2).unsafeRun(), boneyard), boneyard)
   }
 }
