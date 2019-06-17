1927ede19882c7813a46e74d8e62fcdfe785e826
Tomislav Nikolov <tomi_berk@abv.bg>
2019-06-17 11:41:32 +0300

    Update Deck.scala

--- Deck.scala
+++ Deck.scala
@@ -1,28 +1,28 @@
 package drawdominoesgame
 
-import scala.collection.mutable.ArrayBuffer
 
-
-class Deck(private val buffer: ArrayBuffer[Tile]) {
+class Deck(private val buffer: List[Tile]) {
   def isEmpty: Boolean = buffer.isEmpty
 
   def length: Int = buffer.length
 
   def last: Tile = buffer.last
 
-  def access: ArrayBuffer[Tile] = this.buffer
+  def head: Tile = buffer.head
 
-  def add(elem: Tile): ArrayBuffer[Tile] = buffer += elem
+  def access: List[Tile] = this.buffer
 
-  def use(elem: Tile): Tile = {
-    buffer -= elem
-    elem
+  def add(elem: Tile): Deck = Deck(buffer :+ elem)
+
+  def remove(value: Tile, buf: List[Tile] = this.buffer): List[Tile] = buf match {
+    case `value` :: tail =>  tail
+    case x :: tail => x :: remove(value, tail)
+    case _ => Nil
   }
 
-  def moveToEnd(elem: Tile): Tile = {
-    this.add(this.use(elem))
-    buffer.last
-  }
+  def use(elem: Tile): Deck = Deck(remove(elem))
+
+  def moveToEnd(elem: Tile): Deck = this.use(elem).add(elem)
 
   def indexOf(elem: Tile): Int = buffer.indexOf(elem)
 
@@ -40,6 +40,6 @@ class Deck(private val buffer: ArrayBuffer[Tile]) {
 }
 
 object Deck {
-  def apply(b: ArrayBuffer[Tile]): Deck = new Deck(b)
+  def apply(b: List[Tile]): Deck = new Deck(b)
 }
 
