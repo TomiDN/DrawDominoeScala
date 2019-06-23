bfe78ca34f0ce7d2545bd15bdf039b1845468ea1
Tomislav Nikolov <tomi_berk@abv.bg>
2019-06-23 14:28:38 +0300

    Update Game.scala

--- Game.scala
+++ Game.scala
@@ -8,6 +8,8 @@ class Game(val currentPlayer: Player,
            val otherPlayer: Player,
            val boneyard: Deck,
            val openends: Deck) {
+  val emptySpace: Int = 7
+
   def highScoreSystem: HighScoring =
     HighScoring("DDHighScores.txt",
                 s"[-S-Y-S-A-D-M-I-N-] DRAW DOMINOES HIGH SCORES\n\n" +
@@ -77,26 +79,26 @@ class Game(val currentPlayer: Player,
 
   def lastEnd: Tile = openends.last
 
-  def noMove: String = graphics(lastEnd.a) + graphics(lastEnd.a) + graphics(lastEnd.b)
+  val emptyScreen: String = graphics(emptySpace) + graphics(emptySpace)
 
-  def emptyScreen: String = graphics(7) + graphics(7)
+  val wrongMove: String = Game.commandLine + s"Tile ends dismatch! Try again!\n"
 
-  def unsuccessfulpassMove: String =
+  val unsuccessfulpassMove: String =
         Game.commandLine + s"Boneyard still not empty and you can't pass a move!\n" +
         Game.commandLine + s"A new tile will be added to your pile instead!\n"
 
+  def noMove: String = graphics(lastEnd.a) + graphics(lastEnd.a) + graphics(lastEnd.b)
+
   def openEndDisplay: String =
-        graphics(lastEnd.a) + graphics(lastEnd.b) + graphics(7) +
+        graphics(lastEnd.a) + graphics(lastEnd.b) + graphics(emptySpace) +
         Game.commandLine + s"There are ${openends.length - 1} more open ends to pick from.\n"
 
-  def firstMove(t: Tile): String = graphics(t.a) + graphics(t.b) + graphics(7)
-
-  def wrongMove(): String = Game.commandLine + s"Tile ends dismatch! Try again!\n"
+  def firstMove(t: Tile): String = graphics(t.a) + graphics(t.b) + graphics(emptySpace)
 
   def normalMove(t: Tile): String = {
     val last: String = graphics(lastEnd.b)
-    if (t.a == lastEnd.b) last + graphics(t.a) + graphics(t.b) + graphics(7)
-    else last + graphics(t.b) + graphics(t.a) + graphics(7)
+    if (t.a == lastEnd.b) last + graphics(t.a) + graphics(t.b) + graphics(emptySpace)
+    else last + graphics(t.b) + graphics(t.a) + graphics(emptySpace)
   }
 
   def playerInfo: String = currentPlayer.announcePlayer() + currentPlayer.printDeck() + "\n"
@@ -124,7 +126,7 @@ class Game(val currentPlayer: Player,
   def endGame: IO[Unit] = for {
     _ <- putStrLn(Game.commandLine + s"Calculating results...\n\n")
     _ <- checkForWinner
-    _ <- putStrLn(Game.commandLine + s"GOOD GAME, BYE!" + graphics(7))
+    _ <- putStrLn(Game.commandLine + s"GOOD GAME, BYE!" + graphics(emptySpace))
   }yield ()
 }
 
@@ -159,20 +161,20 @@ case class simpleSwitchGame(gameEntity: Game) extends
         gameEntity.openends)
 
 object Game {
-  def commandLine: String = s"dominogame:~  "
+  val commandLine: String = s"dominogame:~  "
 
-  def pickLine: String = commandLine + s" Pick:"
+  val pickLine: String = commandLine + s" Pick:"
 
-  def nextMove: String = "-ne"
+  val nextMove: String = "-ne"
 
-  def drawTile: String = "-dt"
+  val drawTile: String = "-dt"
 
-  def passMove: String = "-pm"
+  val passMove: String = "-pm"
 
-  def quitGame: String = "-q"
+  val quitGame: String = "-q"
 
-  def instructions(): IO[Unit] =
-    putStrLn( s"////////////////////////////////////////////////////////////////////////////////" +
+  val instructions: String =
+                    s"////////////////////////////////////////////////////////////////////////////////" +
                   s"\n////////////////////////////////   Welcome to:   ///////////////////////////////" +
                   s"\n////////////////////   [-S-Y-S-A-D-M-I-N-] DRAW DOMINOES   /////////////////////" +
                   s"\n////////////////////////////////////////////////////////////////////////////////" +
@@ -182,7 +184,7 @@ object Game {
                   s"\n> [    $nextMove     ] - command for displaying another open end"                 +
                   s"\n> [    $drawTile     ] - command for drawing another tile from the boneyard"      +
                   s"\n> [    $passMove     ] - command for passing a move"                              +
-                  s"\n> [     $quitGame     ] - command for quiting the game\n\n")
+                  s"\n> [     $quitGame     ] - command for quiting the game\n\n"
 
   def createBoneyard(array: List[Tile], count: Int = 0, i: Int = 0, j: Int = 0): List[Tile] =
     if (count < 28) {
@@ -215,6 +217,14 @@ object Game {
     } else if (t.a == t.b) {
       openends.add(t).add(t).add(t)
     } else openends.add(t)
+
+  def startNewGame(name1: String, name2: String): Game = {
+    val initialFirstState: Deck = Game.initialDeck
+    val player1: Player = Player(name1, initialFirstState)
+    val initialSecondState: Deck = Game.initialDeck.diff(player1.pile)
+    val player2: Player = Player(name2, initialSecondState)
+    Game(player1, player2, initialSecondState.diff(player2.pile))
+  }
 
   def apply(currentPlayer: Player, otherPlayer: Player, boneyard: Deck, openends: Deck = Deck(List())): Game =
     new Game(currentPlayer, otherPlayer, boneyard, openends)
