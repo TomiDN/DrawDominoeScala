b11d7fe58b37d478a151e0f9cdf8a7299904d4e8
Tomislav Nikolov <tomi_berk@abv.bg>
2019-06-25 10:23:39 +0300

    Update HighScoring.scala

--- HighScoring.scala
+++ HighScoring.scala
@@ -25,22 +25,15 @@ class HighScoring(hsFile: File, filename: String, printmessage: String, emptyfil
       new PrintWriter(hsFile)
     )
 
-  def initiateFile(writer: PrintWriter): Unit = writer.write("")
-
   def noHighScores: Boolean = Source.fromFile(filename).getLines().toVector.isEmpty
-
-  def fileExists: Boolean =
-    if(!hsFile.exists()) {
-      initiateFile(new PrintWriter(hsFile))
-      false
-    } else true
 
   def printLines(lines: Vector[String], res: String = ""): String =
     if (lines.isEmpty) res
     else printLines(lines.tail, res + lines.head + "\n")
 
   def printResult: IO[Unit] =
-    if (fileExists && noHighScores) putStrLn(emptyfilemessage)
+    if (hsFile.createNewFile()) putStrLn(emptyfilemessage)
+    else if (noHighScores) putStrLn(emptyfilemessage)
     else putStrLn(printLines(Source.fromFile(filename).getLines.toVector))
 
   def printHighScore: IO[Unit] = for {
